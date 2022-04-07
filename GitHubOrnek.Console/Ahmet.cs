63fb346866fdfd312291c3d65011a7569b61271e
AhmetKbpc <resmi.ahmetk@gmail.com>
2022-04-07 09:38:48 +0300

    Add project files.

---
+++ GitHubOrnek.Console/Ahmet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubOrnek.Console;
+
+public class Ahmet
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+}
---
+++ GitHubOrnek.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Emrecan");
+Console.WriteLine("Ali Erhan");
+Console.WriteLine("Mesut");
+Console.WriteLine("Mert");
+Console.WriteLine("Ahmet");
+Console.WriteLine("Can");
+Console.WriteLine("Yağızhan");
+Console.WriteLine("Serkan");
+Console.WriteLine("Yağızhan");
+Console.WriteLine("Can");
+Console.WriteLine("Talha");
+Console.WriteLine("Elif");
+Console.WriteLine("Talha");
+Console.WriteLine("C");
+Console.WriteLine("A");
+Console.WriteLine("N");
+Console.WriteLine("Talha");
+Console.WriteLine("Sema");
+Console.WriteLine("Doğukan");
+Console.WriteLine("Çaça");
